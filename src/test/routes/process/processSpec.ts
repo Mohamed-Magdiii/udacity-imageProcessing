0d25c830c5ad77bf857c3d4f524ca0c506d1f994
Mohamed Magdy <Mohamed.Magdy2@alfuttaim.com>
2022-08-30 12:26:15 +0200

    image-processing-udacity

--- src/test/routes/process/processSpec.ts
+++ src/test/routes/process/processSpec.ts
@@ -7,13 +7,11 @@ describe('Testing Image Api endpoint', () => {
   it('get Api without providing the name parameter returns 404', async () => {
     await request.get('/api/v1/imageProcess').expect(404);
   });
-//   it('Using the endpoint with a non-existent image returns 404', async () => {
-//     await request.get('/api/v1/imageProcessing?name=fjord').expect(404);
-//   });
+
   it('get Api with valid path image, but with invalid width and height returns 404', async () => {
     await request.get('/api/v1/imageProcess?name=Desert').expect(404);
   });
-  it('Using the endpoint with a valid img and width and height returns 200', async () => {
+  it('get Api with valid with a valid img and width and height returns 200', async () => {
     await request.get('/api/v1/imageProcess?name=Desert&w=500&h=200').expect(200);
   });
 });
